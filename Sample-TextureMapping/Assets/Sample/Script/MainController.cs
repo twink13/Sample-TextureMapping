dd8062f63eaf2e28497fe32f6e9b19e7fffe8d74
twink13 <twink1313@me.com>
2017-08-27 23:44:18 +0800

    add feature: snapshot saving

--- Sample-TextureMapping/Assets/Sample/Script/MainController.cs
+++ Sample-TextureMapping/Assets/Sample/Script/MainController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Rendering;
+using System.IO;
 
 public class MainController : MonoBehaviour {
 
@@ -37,14 +38,22 @@ public class MainController : MonoBehaviour {
         GUILayout.BeginArea(new Rect(200, 0, 200, 200));
         if (GUILayout.Button("Render TextureMapping!"))
         {
+            // get info
             Mesh mesh = TargetRenderer.sharedMesh;
             Texture mainTex = TargetRenderer.sharedMaterial.mainTexture;
 
+            // setup command buffer
             m_TextureMappingDrawMaterial.mainTexture = mainTex;
             m_CommandBuffer.DrawMesh(mesh, Matrix4x4.identity, m_TextureMappingDrawMaterial);
 
+            // excute render
             Graphics.SetRenderTarget(m_RenderTexture);
             Graphics.ExecuteCommandBuffer(m_CommandBuffer);
+
+            // save snapshot as result
+            Texture2D tex2d = new Texture2D(1024, 1024);
+            tex2d.ReadPixels(new Rect(0, 0, 1024, 1024), 0, 0);
+            File.WriteAllBytes("Assets/Sample/Out/TextureMapping-Result.png", tex2d.EncodeToPNG());
         }
         GUILayout.EndArea();
     }
